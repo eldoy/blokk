0a125be307acf2381c2e05de7afc66c0f4b30848
Vidar Eldøy <vidar@eldoy.com>
2022-05-01 20:17:39 +0200

    Add tests for list

--- spec/tests/details.test.js
+++ spec/tests/details.test.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-
 const details = require('../../lib/details.js')
 
 const it = {}, x = {}
@@ -32,22 +31,22 @@ it['should render edit link'] = async function() {
 
 it['should render delete link'] = async function() {
   const result = await details({
-    delete: 'delete'
+    del: 'delete'
   })
   assert.equal(result, '<div class="details"><nav>delete</nav><h2>Details</h2></div>')
 }
 
-it['should render item details as object'] = async function() {
+it['should render object'] = async function() {
   const result = await details({
-    item: { name: 'hello', email: 'hello@example.com' }
+    render: { name: 'hello', email: 'hello@example.com' }
   })
   assert.equal(result, '<div class="details"><h2>Details</h2><dl><dt>name</dt><dd>hello</dd><dt>email</dt><dd>hello@example.com</dd></dl></div>')
 }
 
-it['should render item details as function'] = async function() {
+it['should render function'] = async function() {
   const user = { name: 'hello', email: 'hello@example.com' }
   const result = await details({
-    item: function() {
+    render: function() {
       return `name: ${user.name}, email: ${user.email}`
     }
   })
--- spec/tests/list.test.js
+++ spec/tests/list.test.js
@@ -1,9 +1,119 @@
+const assert = require('assert')
 const list = require('../../lib/list.js')
 
 const it = {}, x = {}
 
-it['should'] = async function() {
+it['should render default list'] = async function() {
+  const result = await list()
+  assert.equal(result, '<div class="list"><h2>List</h2></div>')
+}
 
+it['should render custom title'] = async function() {
+  const result = await list({
+    title: 'Hello'
+  })
+  assert.equal(result, '<div class="list"><h2>Hello</h2></div>')
+}
+
+it['should render description'] = async function() {
+  const result = await list({
+    desc: 'hello'
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><p>hello</p></div>')
+}
+
+it['should render create link'] = async function() {
+  const result = await list({
+    create: 'create'
+  })
+  assert.equal(result, '<div class="list"><nav>create</nav><h2>List</h2></div>')
+}
+
+it['should render items list as object, item only'] = async function() {
+  const users = [
+    { name: 'hello', email: 'hello@example.com' },
+    { name: 'bye', email: 'bye@example.com' }
+  ]
+  const result = await list({
+    render: users,
+    item: async function(user) {
+      return `item: ${user.name},`
+    }
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><ul><li>item: hello,</li><li>item: bye,</li></ul></div>')
+}
+
+it['should render items list as object, item and show'] = async function() {
+  const users = [
+    { name: 'hello', email: 'hello@example.com' },
+    { name: 'bye', email: 'bye@example.com' }
+  ]
+  const result = await list({
+    render: users,
+    item: async function(user) {
+      return `item: ${user.name},`
+    },
+    show: async function(user) {
+      return `show: ${user.name},`
+    }
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><ul><li>item: hello,<nav>show: hello,</nav></li><li>item: bye,<nav>show: bye,</nav></li></ul></div>')
+}
+
+it['should render items list as object, item, edit and del'] = async function() {
+  const users = [
+    { name: 'hello', email: 'hello@example.com' },
+    { name: 'bye', email: 'bye@example.com' }
+  ]
+  const result = await list({
+    render: users,
+    item: async function(user) {
+      return `item: ${user.name},`
+    },
+    edit: async function(user) {
+      return `edit: ${user.name},`
+    },
+    del: async function(user) {
+      return `del: ${user.name},`
+    }
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><ul><li>item: hello,<nav>edit: hello,del: hello,</nav></li><li>item: bye,<nav>edit: bye,del: bye,</nav></li></ul></div>')
+}
+
+it['should render items list as object, all options'] = async function() {
+  const users = [
+    { name: 'hello', email: 'hello@example.com' },
+    { name: 'bye', email: 'bye@example.com' }
+  ]
+  const result = await list({
+    render: users,
+    item: async function(user) {
+      return `item: ${user.name},`
+    },
+    show: async function(user) {
+      return `show: ${user.name},`
+    },
+    edit: async function(user) {
+      return `edit: ${user.name},`
+    },
+    del: async function(user) {
+      return `del: ${user.name},`
+    }
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><ul><li>item: hello,<nav>show: hello,edit: hello,del: hello,</nav></li><li>item: bye,<nav>show: bye,edit: bye,del: bye,</nav></li></ul></div>')
+}
+
+it['should render items list as function'] = async function() {
+  const users = [
+    { name: 'hello', email: 'hello@example.com' },
+    { name: 'bye', email: 'bye@example.com' }
+  ]
+  const result = await list({
+    render: function() {
+      return `<ul>${users.map(user => `<li>name: ${user.name}, email: ${user.email}</li>`).join('')}</ul>`
+    }
+  })
+  assert.equal(result, '<div class="list"><h2>List</h2><ul><li>name: hello, email: hello@example.com</li><li>name: bye, email: bye@example.com</li></ul></div>')
 }
 
 module.exports = it
