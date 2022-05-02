95287a625088cabf942c67b0969759ba880f04df
Vidar Eldøy <vidar@eldoy.com>
2022-05-02 04:18:09 +0200

    Add form port from 7i

--- spec/tests/form.test.js
+++ spec/tests/form.test.js
@@ -3,8 +3,17 @@ const form = require('../../lib/form.js')
 
 const it = {}, x = {}
 
-it['should'] = async function() {
+it['should render an empty form'] = async function() {
+  const result = await form()
+  assert.equal(result, `<form onsubmit="return false"></form>`)
+}
 
+it['should render a form with buttons'] = async function() {
+  const result = await form({
+    button: async function() { return 'hello' }
+  })
+
+  assert.equal(result, '<form onsubmit="return false">hello</form>')
 }
 
 module.exports = it
