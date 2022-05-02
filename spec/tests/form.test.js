9ab5b83a0936cd989fb768a63073a1391a556484
Vidar Eldøy <vidar@eldoy.com>
2022-05-02 13:40:03 +0200

    Add form tests

--- spec/tests/form.test.js
+++ spec/tests/form.test.js
@@ -1,19 +1,50 @@
 const assert = require('assert')
+const parser = require('node-html-parser')
 const form = require('../../lib/form.js')
 
 const it = {}, x = {}
 
 it['should render an empty form'] = async function() {
   const result = await form()
-  assert.equal(result, `<form onsubmit="return false"></form>`)
+  assert.equal(result, `<form onsubmit="return false"><p><button onclick="handleSave(this)">Save</button><a href="javascript:void(0)" onclick="goBack()">Cancel</a></p></form>`)
 }
 
-it['should render a form with buttons'] = async function() {
+it['should render a form with button as function'] = async function() {
   const result = await form({
     button: async function() { return 'hello' }
   })
+  const doc = parser.parse(result)
+  const f = doc.querySelector('form')
+  assert.equal(f.getAttribute('onsubmit'), 'return false')
+  assert.equal(f.text, 'hello')
+}
 
-  assert.equal(result, '<form onsubmit="return false">hello</form>')
+it['should render a form with button as string'] = async function() {
+  const result = await form({
+    button: 'hello'
+  })
+  const doc = parser.parse(result)
+  const f = doc.querySelector('form')
+  assert.equal(f.getAttribute('onsubmit'), 'return false')
+  assert.equal(f.text, 'hello')
+}
+
+it['should render a text input from fields'] = async function() {
+  const result = await form({
+    fields: [{ name: 'hello' }]
+  })
+  const doc = parser.parse(result)
+  const f = doc.querySelector('form')
+  assert.equal(f.innerHTML, `<p><label for="hello">hello</label><br><input id="hello" type="text" name="hello" oninput="clearErrors(this)" data-default=""><em class="hello-errors"></em></p><p><button onclick="handleSave(this)">Save</button><a href="javascript:void(0)" onclick="goBack()">Cancel</a></p>`)
+}
+
+it['should render a text area from fields'] = async function() {
+  const result = await form({
+    fields: [{ name: 'hello', type: 'textarea' }]
+  })
+  const doc = parser.parse(result)
+  const f = doc.querySelector('form')
+  assert.equal(f.innerHTML, `<p><label for="hello">hello</label><br><textarea id="hello" name="hello" oninput="clearErrors(this)" data-default=""></textarea><em class="hello-errors"></em></p><p><button onclick="handleSave(this)">Save</button><a href="javascript:void(0)" onclick="goBack()">Cancel</a></p>`)
 }
 
 module.exports = it
