cd2142ded8218b9671aea193e144591f80d38f47
Vidar Eldøy <vidar@eldoy.com>
2022-05-19 01:32:32 +0200

    No need for list el

--- app/pages/demo.js
+++ app/pages/demo.js
@@ -10,34 +10,31 @@ module.exports = async function($) {
 
   return /* html */`
     <h1>Demo</h1>
-    <div id="project-list">
-      ${list({
-        el: '#project-list',
-        title: 'Projects',
-        size: 2,
-        items: projects,
-        limit: 10,
-        handleFetch: function({ sort, skip, limit }) {
-          return api('/project/find', {
-            query: {},
-            skip,
-            limit,
-            sort
-          })
-        },
-        handleSearch: function({ search, sort, skip, limit }) {
-          return api('/project/search', {
-            query: {},
-            search,
-            sort,
-            skip,
-            limit
-          })
-        },
-        renderInfo: function(item) {
-          return `<a href="/project/show?project_id=${item.id}">${item.title} (${item.id})</a>`
-        }
-      })}
-    </div>
+    ${list({
+      title: 'Projects',
+      size: 2,
+      items: projects,
+      limit: 10,
+      handleFetch: function({ sort, skip, limit }) {
+        return api('/project/find', {
+          query: {},
+          skip,
+          limit,
+          sort
+        })
+      },
+      handleSearch: function({ search, sort, skip, limit }) {
+        return api('/project/search', {
+          query: {},
+          search,
+          sort,
+          skip,
+          limit
+        })
+      },
+      renderInfo: function(item) {
+        return `<a href="/project/show?project_id=${item.id}">${item.title} (${item.id})</a>`
+      }
+    })}
   `
 }
